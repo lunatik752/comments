0ffff1c2f5489e9344529b221a926f8406407dec
lunatik752 <whitemaxim752@gmail.com>
2020-12-29 23:28:38 +0300

    fixed App

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import {PostPage} from "./pages/PostPage";
 import {Provider} from "react-redux";
-import {AppStateType} from "./state/store";
 
 const App: React.FC<{store: any}> = (props) => {
     return (
