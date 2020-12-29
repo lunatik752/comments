83bf420034c94dd8b815c0bc9538b641e2ec68e9
lunatik752 <whitemaxim752@gmail.com>
2020-12-29 23:23:40 +0300

    add thunk fetchPosts

--- src/features/posts/bll/reducer.ts
+++ src/features/posts/bll/reducer.ts
@@ -1,9 +1,30 @@
-import {PostType} from "../../../api/api";
+import {api, PostType} from "../../../api/api";
+import {Dispatch} from "redux";
 
 const initialState = {
     items: [] as PostType[]
 }
 
-export const postReducer = (state = initialState, action: any) => {
+export const postReducer = (state = initialState, action: PostReducerActionsType) => {
+    switch (action.type) {
+        case "FETCH_POST_SUCCESS": {
+            return {
+                ...state, items: action.payload.posts
+            }
+        }
+    }
     return state
+}
+
+type PostReducerActionsType = ReturnType<typeof fetchPostSuccess>
+
+export const fetchPostSuccess = (posts: PostType[]) => ({
+    type: 'FETCH_POST_SUCCESS',
+    payload: {posts}
+ } as const)
+
+
+export const fetchPosts = () => async (dispatch: Dispatch) => {
+    const posts = await api.getPosts()
+    dispatch(fetchPostSuccess(posts))
 }
--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,16 +1,22 @@
-import React from "react";
-import {useSelector} from "react-redux";
-import { PostType } from "../api/api";
-import { Post } from "../features/posts/components/Post";
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {PostType} from "../api/api";
+import {Post} from "../features/posts/components/Post";
 import {AppStateType} from "../state/store";
+import {fetchPosts} from "../features/posts/bll/reducer";
 
 export const PostPage: React.FC = () => {
 
     const items = useSelector<AppStateType, PostType[]>(state => state.posts.items)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(fetchPosts())
+    }, [dispatch])
 
     return (
         <div>
-            {items.map((i: PostType) => <Post key={i.id} post={i}/> )}
+            {items.map((i: PostType) => <Post key={i.id} post={i}/>)}
         </div>
     )
 }
