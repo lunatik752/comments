38cdd644e88794980bc3b823367d6b4aa39f77ed
lunatik752 <whitemaxim752@gmail.com>
2021-01-03 20:52:18 +0300

    changed Post

--- src/features/posts/components/Post.tsx
+++ src/features/posts/components/Post.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import {PostType} from "../../../api/api";
+import {useDispatch} from "react-redux";
+import {updatePosts} from "../bll/reducer";
 
 
 export const Post: React.FC<{ post: PostType }> = ({post}) => {
+
+    const [editMode, setEditMode] = useState(false)
+    const [text, setText] = useState(post.text)
+    const dispatch = useDispatch()
+
+   const changeText = (e: ChangeEvent<HTMLTextAreaElement>) => {
+       setText(e.currentTarget.value)
+   }
+   const onBlurHandle = () => {
+       dispatch(updatePosts(post.id, text))
+       setEditMode(false)
+   }
+
     return (
         <div>
             <b>{post.author.name}</b>
-            <span>{post.text}</span>
+            <br/>
+            {!editMode
+                ? <span onDoubleClick={() => setEditMode(true)}>{post.text}</span>
+                : <textarea
+                    onChange={changeText}
+                    onBlur={onBlurHandle}
+                >{text}</textarea>}
+                <br/>
+                Likes: {post.likes}
             <hr/>
         </div>
     );
